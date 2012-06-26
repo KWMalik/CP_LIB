32385e0a91c2c62b4a860936c0e4ca587e755f0f
JustinFeng <realfengjia@foxmail.com>
2012-06-26 09:04:51 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrownePlazaLib::Application.config.secret_token = '778e6ea8acd455b4b1d9e55cb984f2fcf38f3fd99c48aa707d87cdaf68345f01fd008dd0b9a7fb232754f1dad180a251777ac5a168597296453a7f61ae8c197e'
